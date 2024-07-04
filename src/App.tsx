747c1e2fa2f2059aeb5ec83d7da8a3dfd698bda2
sinan-hasil <155896141+sinan-hasil@users.noreply.github.com>
2024-07-04 19:43:58 +0300

    asdad

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { useState } from "react";
 import { Button, Container } from "react-bootstrap";
 import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
